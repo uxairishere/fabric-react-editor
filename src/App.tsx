eeaf7e6890e24d5e0ea11fed16f7a2eac23a0ed3
Uxair Abbas <uzaaaaaair@gmail.com>
2023-06-04 07:21:59 +0500

    added more features

--- src/App.tsx
+++ src/App.tsx
@@ -3,17 +3,19 @@ import { fabric } from "fabric"; // this also installed on your project
 import { FabricJSCanvas, useFabricJSEditor } from "fabricjs-react";
 import "./App.css";
 import default_bg from './assets/defaultbg.avif';
-import { addImage, onAddCircle, onAddRectangle, onAddText, onDeleteAll, onDeleteSelected } from "./components/fabricHooks";
+import { addImage, onAddCircle, onAddRectangle, onAddText, onBackgroundChange, onDeleteAll, onDeleteSelected, onFillObject } from "./components/fabricHooks";
 
-import { BsCircle, BsChatLeftText, BsFillImageFill } from 'react-icons/bs'
+import { BsCircle, BsChatLeftText, BsFillImageFill, BsGrid3X3GapFill, BsCreditCard2Front } from 'react-icons/bs'
 import { BiRectangle } from 'react-icons/bi'
 import { TbRectangleFilled } from 'react-icons/tb'
 import { RxReset } from 'react-icons/rx'
 import { RiDeleteBin5Line } from 'react-icons/ri'
-import { MdAdd } from 'react-icons/md'
+import { MdAdd, MdOutlineColorLens } from 'react-icons/md'
 import { GoDesktopDownload } from 'react-icons/go'
-
-// const DEFAULT_IMAGE = "example.jpg";
+import { FaSquare } from 'react-icons/fa'
+import { ColorPicker, useColor } from "react-color-palette";
+import { FcEditImage } from 'react-icons/fc'
+import "react-color-palette/lib/css/styles.css";
 
 export default function App() {
   const [backgroundImage, setBackgroundImage] = useState<any>(null);
@@ -21,22 +23,23 @@ export default function App() {
   const [text, setText] = useState<any>("");
   const [showTextField, setShowTextField] = useState<boolean>(false)
   const [showUploadField, setShowUploadField] = useState<boolean>(false)
+  const [showBGField, setShowBGField] = useState<boolean>(false)
+
+  const [showPallate, setShowPallate] = useState<boolean>(false)
+
+
+  const [color, setColor] = useColor("hex", "#d946ef");
 
   const { selectedObjects, editor, onReady } = useFabricJSEditor();
 
-  // useEffect(() => {
-  //   setBackgroundImage(default_bg);
-  //   console.log(backgroundImage)
-  // }, [backgroundImage])
-
-  console.log("IUIMAGEINF: ", backgroundImage)
   const _onReady = (canvas: any) => {
-    let DEFAULT_IMAGE = backgroundImage
-    fabric.Image.fromURL(DEFAULT_IMAGE, (img) => {
+    // setBackgroundImage(default_bg)
+
+    fabric.Image.fromURL(backgroundImage, (img) => {
       console.log("editor canvas: ", canvas);
       console.log("editor image: ", img);
 
-      canvas.set("backgroundImage", img);
+      canvas.set("backgroundColor", '#fafaf9');
       canvas.renderAll();
       onReady(canvas);
     });
@@ -46,116 +49,143 @@ export default function App() {
     if (editor) {
       const canvas = editor?.canvas;
       if (canvas) {
-        // Convert canvas to data URL
         const dataURL = canvas.toDataURL({
           format: "png",
-          multiplier: 2, // Increase multiplier for higher resolution
+          multiplier: 2, 
         });
 
-        // Create a temporary link element
         const link = document.createElement("a");
         link.href = dataURL;
         link.download = "canvas.png";
 
-        // Simulate a click event to trigger the download
         link.click();
       }
     }
   }
 
   return (
-    <div className="w-fit mx-auto">
-      <div className="flex justify-start items-start">
-        <div className="flex flex-col gap-y-3 text-3xl p-3">
-          <button
-            className="border border-zinc-200 rounded-lg p-2 w-fit hover:bg-cyan-50 hover:text-cyan-500 hover:border-cyan-500 transition-all"
-            onClick={() => onAddCircle(editor)}>
-            <BsCircle />
-          </button>
-          <button
-            className="border border-zinc-200 rounded-lg p-2 w-fit hover:bg-cyan-50 hover:text-cyan-500 hover:border-cyan-500 transition-all"
-            onClick={() =>
-              onAddRectangle(editor)}>
-            <BiRectangle />
-          </button>
+    <div className="relative">
+      <div className="bg-dotted h-[20rem] w-[20rem] absolute -z-10 left-0 top-20"></div>
+      <div className="bg-dotted h-[20rem] w-[20rem] absolute -z-10 right-0 -bottom-14"></div>
+      <div className="w-fit mx-auto">
+        <div className="flex justify-center items-center text-white text-2xl uppercase font-thin text-center py-10 tracking-widest gap-x-3">
+          <h1><FcEditImage /></h1>
+          <h1>edit with fabric and react</h1>
+        </div>
+        <div className="flex justify-start items-start">
+          <div className="flex flex-col gap-y-3 text-3xl p-3">
+            <button
+              className="bg-white/10 text-white  rounded-lg p-2 w-fit hover:bg-cyan-500/10 hover:text-cyan-500 hover:border-cyan-500 transition-all"
+              onClick={() => onAddCircle(editor)}>
+              <BsCircle />
+            </button>
+            <button
+              className="bg-white/10 text-white rounded-lg p-2 w-fit hover:bg-cyan-500/10 hover:text-cyan-500 hover:border-cyan-500 transition-all"
+              onClick={() =>
+                onAddRectangle(editor)}>
+              <BiRectangle />
+            </button>
 
-          {/* add text  */}
-          <div className="relative">
-            <button
-              onClick={() => setShowTextField(!showTextField && true)}
-              className=" border border-zinc-200 rounded-lg p-2 w-fit hover:bg-cyan-50 hover:text-cyan-500 hover:border-cyan-500 transition-all"
-            >
-              <BsChatLeftText />
-            </button>
-            {showTextField &&
-              <fieldset className="absolute z-10 top-0 -right-[14rem] flex justify-start items-center gap-x-1">
-                <input
-                  className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500  p-1.5 "
-                  name={`text`}
-                  type={`text`}
-                  value={text}
-                  onChange={(event) => setText(event.target.value)}
-                />
-                <button
-                  className="border border-zinc-200 rounded-full p-2 w-fit hover:bg-cyan-50 hover:text-cyan-500 hover:border-cyan-500 transition-all"
-                  onClick={() => onAddText(editor, text, setText, setShowTextField)}>
-                  <MdAdd />
-                </button>
-              </fieldset>
-            }
-          </div>
-          {/* add image  */}
-          <div className="relative">
-            <button
-              className="border border-zinc-200 rounded-lg p-2 w-fit hover:bg-cyan-50 hover:text-cyan-500 hover:border-cyan-500 transition-all"
-              onClick={() => setShowUploadField(prev => !prev)}>
-              <BsFillImageFill />
-            </button>
-            {showUploadField &&
-              <fieldset className="absolute z-10 top-0 -right-[21rem] flex justify-start items-center gap-x-1">
-                <input
-                  className="text-sm text-slate-500
+            {/* add text  */}
+            <div className="relative">
+              <button
+                onClick={() => setShowTextField(!showTextField && true)}
+                className="bg-white/10 text-white  rounded-lg p-2 w-fit hover:bg-cyan-500/10 hover:text-cyan-500 hover:border-cyan-500 transition-all"
+              >
+                <BsChatLeftText />
+              </button>
+              {showTextField &&
+                <fieldset className="absolute z-10 top-0 -right-[15rem] flex justify-start items-center gap-x-1">
+                  <input
+                    className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500  p-1.5 "
+                    name={`text`}
+                    type={`text`}
+                    value={text}
+                    onChange={(event) => setText(event.target.value)}
+                  />
+                  <button
+                    className="border border-zinc-200 rounded-full p-2 w-fit hover:bg-cyan-50 hover:text-cyan-500 hover:border-cyan-500 transition-all"
+                    onClick={() => onAddText(editor, text, setText, setShowTextField)}>
+                    <MdAdd />
+                  </button>
+                </fieldset>
+              }
+            </div>
+
+            {/*  fill object color  */}
+            <div className="relative">
+              <button
+                className="bg-white/10 text-white  rounded-lg p-2 w-fit hover:bg-cyan-500/10 hover:text-cyan-500 hover:border-cyan-500 transition-all"
+                onClick={() => setShowPallate(prev => !prev)}>
+                <MdOutlineColorLens style={{ fill: `${color.hex}` }} />
+              </button>
+              {showPallate &&
+                <div className="absolute top-0 -right-[33.5rem] z-10 flex justify-start items-start gap-x-2">
+                  <ColorPicker width={456} height={228} color={color} onChange={setColor} hideHSV dark />
+                  <button
+                    className="border border-zinc-200 rounded-full p-2 w-fit hover:bg-cyan-50 hover:text-cyan-500 hover:border-cyan-500 transition-all"
+                    onClick={() => onFillObject(editor, color)}>
+                    <MdAdd />
+                  </button>
+                </div>}
+            </div>
+
+            {/* add image  */}
+            <div className="relative">
+              <button
+                className="bg-white/10 text-white  rounded-lg p-2 w-fit hover:bg-cyan-500/10 hover:text-cyan-500 hover:border-cyan-500 transition-all"
+                onClick={() => setShowUploadField(prev => !prev)}>
+                <BsFillImageFill />
+              </button>
+              {showUploadField &&
+                <fieldset className="absolute z-10 top-0 -right-[21rem] flex justify-start items-center gap-x-1">
+                  <input
+                    className="text-sm text-slate-500
                   file:mr-4 file:py-2 file:px-4
                   file:rounded-full file:border-0
                   file:text-sm file:font-semibold
                   file:bg-violet-50 file:text-violet-700
                   hover:file:bg-violet-100"
-                  type="file"
-                  onChange={(event) => { if (event.target.files) setObjectImage(event.target.files[0]) }}
-                />
-                <button
-                  className="border border-zinc-200 rounded-full p-2 w-fit hover:bg-cyan-50 hover:text-cyan-500 hover:border-cyan-500 transition-all"
-                  onClick={() => addImage(fabric, editor, objectImage, setShowUploadField)}>
-                  <MdAdd />
-                </button>
-              </fieldset>
-            }
+                    type="file"
+                    onChange={(event) => { if (event.target.files) setObjectImage(event.target.files[0]) }}
+                  />
+                  <button
+                    className="border border-zinc-200 rounded-full p-2 w-fit hover:bg-cyan-50 hover:text-cyan-500 hover:border-cyan-500 transition-all"
+                    onClick={() => addImage(fabric, editor, objectImage, setShowUploadField)}>
+                    <MdAdd />
+                  </button>
+                </fieldset>
+              }
+            </div>
+
+            {/* revert changes */}
+            <button
+              className="bg-white/10 text-white rounded-lg p-2 w-fit hover:bg-cyan-500/10 hover:text-cyan-500 hover:border-cyan-500 transition-all"
+              onClick={() => onDeleteAll(editor, setText)}>
+              <RxReset />
+            </button>
+
+            {/* Delete selected  */}
+            <button
+              className="bg-white/10  text-red-500 rounded-lg p-2 w-fit hover:bg-red-500/10 hover:text-red-500 hover:border-red-500 transition-all"
+              onClick={() => onDeleteSelected(editor)}>
+              <RiDeleteBin5Line />
+            </button>
+            {/* download  */}
+            <button
+              className="bg-white/10 rounded-lg p-2 w-fit text-green-500 hover:bg-green-500/10 hover:text-green-500 hover:green-cyan-500 transition-all"
+              onClick={download}
+            >
+              <GoDesktopDownload />
+            </button>
           </div>
-          {/* revert changes */}
-          <button
-            className="border border-zinc-200 rounded-lg p-2 w-fit hover:bg-cyan-50 hover:text-cyan-500 hover:border-cyan-500 transition-all"
-            onClick={() => onDeleteAll(editor, setText)}>
-            <RxReset />
-          </button>
-
-          {/* Delete selected  */}
-          <button
-            className="border border-red-200 text-red-500 rounded-lg p-2 w-fit hover:bg-red-50 hover:text-red-500 hover:border-red-500 transition-all"
-            onClick={() => onDeleteSelected(editor)}>
-            <RiDeleteBin5Line />
-          </button>
-          {/* download  */}
-          <button
-            className="border border-green-200 rounded-lg p-2 w-fit text-green-500 hover:bg-green-50 hover:text-green-500 hover:green-cyan-500 transition-all"
-            onClick={download}
-          >
-            <GoDesktopDownload />
-          </button>
+          <div className=" border border-zinc-200 rounded-lg">
+            <FabricJSCanvas className="h-[30rem] w-[70rem]" onReady={_onReady} />
+          </div>
         </div>
-        <div className=" border border-zinc-200 rounded-lg">
-          <FabricJSCanvas className="h-[30rem] w-[70rem]" onReady={_onReady} />
+        <div>
+          <h1 className="pt-10 text-white/50 tracking-widest text-center">@uxair_abbass</h1>
         </div>
-
       </div>
     </div>
   );
--- src/components/fabricHooks.js
+++ src/components/fabricHooks.js
@@ -17,6 +17,16 @@ export const onDeleteSelected = (editor) => {
   editor?.deleteSelected();
 };
 
+export const onFillObject = (editor, color) => {
+    editor?.setFillColor(color?.hex)
+}
+
+export const onBackgroundChange = (canvas, backgroundImage, setShowBGField) => {
+    // const getImageURL = URL.createObjectURL(backgroundImage);
+    canvas?.setBackgroundColor("#f0f0f0", canvas.renderAll(canvas));
+    setShowBGField(prev => !prev)
+}
+
 export const addImage = (fabric, editor, imageURL, setShowUploadField) => {
   console.log("adding image");
   if (imageURL) {
